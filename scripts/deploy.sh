9d421868f0ce8025fde443d807baf2bd639d07fe
nitcops <nicolasdimartino23@gmail.com>
2026-04-24 10:46:15 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,8 @@ docker run -d --name web1 --network projet-infra nginx
 docker run -d --name web2 --network projet-infra nginx
 
 # 3. Personnalisation des pages (pour le test visuel)
-docker exec web1 bash -c "echo '<h1>Serveur n°1 - Prod A</h1>' > /usr/share/nginx/html/index.html"
-docker exec web2 bash -c "echo '<h1>Serveur n°2 - Prod B</h1>' > /usr/share/nginx/html/index.html"
+docker exec web1 bash -c "echo '<h1>Serveur n1 - Prod A</h1>' > /usr/share/nginx/html/index.html"
+docker exec web2 bash -c "echo '<h1>Serveur n2 - Prod B</h1>' > /usr/share/nginx/html/index.html"
 
 # 4. Récupération des adresses IP des conteneurs
 IP1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' web1)

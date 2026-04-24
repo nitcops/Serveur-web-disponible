1bda515b10a288ac9cd619a83f096f1f2f0db6b1
nitcops <nicolasdimartino23@gmail.com>
2026-04-24 11:10:16 +0200

    Delete scripts/Backup.sh

--- scripts/Backup.sh
+++
@@ -1,7 +0,0 @@
-### Scripts de sauvegarde
-
-#!/bin/bash
-DEST="/backup/$(date +%Y-%m-%d)"
-mkdir -p $DEST
-cp /etc/nginx/sites-available/default $DEST/nginx_conf.bak
-echo "Sauvegarde terminée dans $DEST"
